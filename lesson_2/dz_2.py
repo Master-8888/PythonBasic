feb6ecc24f1b395ff13fe1719c0c9d767f2cf4d2
Pavel <nedogarokps07@gmail.com>
2025-12-26 23:36:52 +0200

    Submitting homewor

--- lesson_2/dz_2.py
+++ lesson_2/dz_2.py
@@ -12,8 +12,42 @@ number3 = int(input("Введите число №3: "))
 average = (number1 + number2 + number3) / 3
 print(f"Середнє трьох чисел {number}, {number}, {number} дорівнює {average}")
 
-
 # 3. “Перетворення хвилин у години”
 
-number_of_minutes = int(input("Введите число №1: "))
+number_of_minutes = int(input("Введіть кількість хвилин: "))
+watch = number_of_minutes // 60
+minutes = number_of_minutes % 60
+print(f"{number_of_minutes} хвилин становить {watch} годин і {minutes} хвилин")
 
+# v4. “Розрахунок знижки”
+
+sum = int(input("Введіть ціну: "))
+discount = int(input("Введіть знижку (%): "))
+total_price = sum - (sum * discount / 100)
+print(f"Ціна зі знижкою: {total_price}")
+
+# 5. “Остання цифра числа”
+
+numbers = int(input("Введіть 3х значное число: "))
+last_digit = numbers % 10
+print(f"Остання цифра числа {numbers} - {last_digit}")
+
+# 6. “Периметр прямокутника”
+
+length = int(input("Введіть довжину прямокутника: "))
+width = int(input("Введіть ширину прямокутника: "))
+perimeter = 2 * (length + width)
+print(f"Периметр прямокутника зі сторонами {length} і {width} дорівнює {perimeter}")
+
+# 7. Виведення числа в стовпчик
+
+numbers = int(input("Введіть число: "))
+first_digit = numbers // 1000
+second_digit = (numbers // 100) % 10
+third_digit = (numbers // 10) % 10
+last_digit = numbers % 10
+
+print(first_digit)
+print(second_digit)
+print(third_digit)
+print(last_digit)
