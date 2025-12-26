5ca152b38bd51d92da31b0fc595e9b5b9fbec6a8
Pavel <nedogarokps07@gmail.com>
2025-12-26 23:27:44 +0200

    Submitting homework

--- lesson_2/dz_2.py
+++ lesson_2/dz_2.py
@@ -15,4 +15,42 @@ print(f"Середнє трьох чисел {number}, {number}, {number} дор
 
 # 3. “Перетворення хвилин у години”
 
-number_of_minutes = int(input("Введите число №1: "))
+number_of_minutes = int(input("Введіть кількість хвилин: "))
+watch = number_of_minutes // 60
+minutes = number_of_minutes % 60
+print(f"{number_of_minutes} хвилин становить {watch} годин і {minutes} хвилин")
+
+# v4. “Розрахунок знижки”
+
+sum = int(input("Введіть ціну: "))
+discount = int(input("Введіть знижку (%): "))
+total_price = sum - (sum * discount / 100)
+print(f"Ціна зі знижкою: {total_price}")
+
+
+# 5. “Остання цифра числа”
+
+numbers = int(input("Введіть 3х значное число: "))
+last_digit = numbers % 10
+print(f"Остання цифра числа {numbers} - {last_digit}")
+
+
+# 6. “Периметр прямокутника”
+
+length = int(input("Введіть довжину прямокутника: "))
+width = int(input("Введіть ширину прямокутника: "))
+perimeter = 2 * (length + width)
+print(f"Периметр прямокутника зі сторонами {length} і {width} дорівнює {perimeter}")
+
+# 7. Виведення числа в стовпчик
+
+numbers = int(input("Введіть число: "))
+first_digit = numbers // 1000
+second_digit = (numbers // 100) % 10
+third_digit = (numbers // 10) % 10
+last_digit = numbers % 10
+
+print(first_digit)
+print(second_digit)
+print(third_digit)
+print(last_digit)
