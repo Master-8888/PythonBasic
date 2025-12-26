35c764eec77fb0a2d514c7576352e5730a16a80d
Pavel <nedogarokps07@gmail.com>
2025-12-26 23:39:47 +0200

    Submitting homeworр

--- lesson_2/dz_2.py
+++ lesson_2/dz_2.py
@@ -4,6 +4,7 @@ number = int(input("Введите число: "))
 square = number ** 2
 print(f"Квадрат числа {number} равен {square}")
 
+
 # 2. “Середнє трьох чисел”
 
 number1 = int(input("Введите число №1: "))
@@ -11,6 +12,7 @@ number2 = int(input("Введите число №2: "))
 number3 = int(input("Введите число №3: "))
 average = (number1 + number2 + number3) / 3
 print(f"Середнє трьох чисел {number}, {number}, {number} дорівнює {average}")
+
 
 # 3. “Перетворення хвилин у години”
 
